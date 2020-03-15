58b2cc37dfc8d0b4d4eaeba1dc609255120eae2f
karthick3018 <karthickraja3018@gmail.com>
2020-03-15 15:45:44 +0530

    additonal changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Karthick's Gatsby`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    pathPrefix: "/gatsby-corona",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
